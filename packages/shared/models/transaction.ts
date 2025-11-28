3458116387b4a045e148c07ad3c16720b7b7f130
Rico Wang <hirico@google.com>
2025-11-28 15:49:14 +0900

    feat: initial data modeling

--- packages/shared/models/transaction.ts
+++ packages/shared/models/transaction.ts
@@ -2,9 +2,41 @@ interface ITransaction {
   amount: number;
   currency: ICurrency;
   date: Date;
-  description: string;
+  description: string | null;
   isTaxDeductable: boolean;
   hasCapitalGains: boolean;
-  merchant: Merchant;
-  serialNumber: string;
+  merchant: Merchant | null;
+  serialNumber: string | null;
+
+  TransactionType: TransactionType;
+}
+
+enum TransactionType {
+  Expenses,
+  Income,
+  Trade,
+}
+
+class Transaction implements ITransaction {
+  amount: number;
+  currency: ICurrency;
+  date: Date;
+  description: string | null;
+  isTaxDeductable: boolean;
+  hasCapitalGains: boolean;
+  merchant: Merchant | null;
+  serialNumber: string | null;
+  TransactionType: TransactionType;
+
+  constructor(amount: number, currency: ICurrency, date: Date, description: string | null, isTaxDeductable: boolean, hasCapitalGains: boolean, merchant: Merchant | null, serialNumber: string | null, TransactionType: TransactionType) {
+    this.amount = amount;
+    this.currency = currency;
+    this.date = date;
+    this.description = description;
+    this.isTaxDeductable = isTaxDeductable;
+    this.hasCapitalGains = hasCapitalGains;
+    this.merchant = merchant;
+    this.serialNumber = serialNumber;
+    this.TransactionType = TransactionType;
+  }
 }
